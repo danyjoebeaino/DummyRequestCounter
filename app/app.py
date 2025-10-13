1fa3f17dc001e6b3907f23c1a6b259453af4148c
danyjoebeaino <danyjoe.beaino@net.usj.edu.lb>
2025-10-13 19:16:56 +0300

    Modify with Flake8 app.py

--- app/app.py
+++ app/app.py
@@ -3,19 +3,22 @@ from redis import Redis
 import os
 import uvicorn
 
+
 app = FastAPI()
 
-# TODO: Configuration from environment variables
+# Configuration from environment variables
 REDIS_HOST = os.getenv("REDIS_HOST", "redis")
 REDIS_PORT = os.getenv("REDIS_PORT", 6379)
 
 redis = Redis(host=REDIS_HOST, port=REDIS_PORT)
+
 
 @app.get("/")
 def hello():
     redis.incr('hits')
     hits = redis.get('hits').decode('utf-8')
     return f"Hello! This page has been visited {hits} times."
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
